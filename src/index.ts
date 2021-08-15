3e161cd40f1ceb88ef27c743ad1eea3b24a3cc7b
Kandrashin Denis <mail@lintest.ru>
2021-08-15 17:33:26 +0300

    Сборка с ошибками

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,14 @@ import * as monaco from 'monaco-editor';
 
 //@ts-ignore
 self.MonacoEnvironment = {
-	getWorkerUrl: function (moduleId, label) {
-		console.log("getWorkerUrl: ", moduleId, label)
-		switch (label) {
-			case 'json':
-				return './json.worker.bundle.js';
-			case 'css':
-			case 'scss':
-			case 'less':
-				return './css.worker.bundle.js';
-			case 'html':
-			case 'handlebars':
-			case 'razor':
-				return './html.worker.bundle.js';
-			case 'typescript':
-			case 'javascript':
-				return './ts.worker.bundle.js';
-			default:
-				return './editor.worker.bundle.js';
-		}
-	}
+	global: true
 };
 
 let div = document.createElement('div');
 document.body.appendChild(div);
 div.style.height = '600px';
-div.id = 'container';
 
-monaco.editor.create(document.getElementById('container'), {
+monaco.editor.create(div, {
 	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
 	language: 'javascript'
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,7 @@ const path = require('path');
 module.exports = {
 	mode: 'development',
 	entry: {
-		app: './src/index.ts',
-		'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
-		'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
-		'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
-		'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
-		'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker'
+		app: './src/index.ts'
 	},
 	output: {
 		globalObject: 'self',
